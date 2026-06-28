a0d761b56e32188fd6b24b85efd226056749392d
Andre <dibstep763@gmail.com>
2026-06-28 12:11:19 +0300

    feat: joining channel using cli

--- network/certificates/network-composing.sh
+++ network/certificates/network-composing.sh
@@ -39,13 +39,13 @@ elif [ "$COMMAND" == "clean" ]; then
     
     echo " Creating and joining channel via admin CLI..."
     # Join the Orderer via host osnadmin
-    ~/go/src/github.com/hyperledger/fabric-samples/bin/osnadmin channel join \
+    docker exec cli osnadmin channel join \
       --channelID glyphchannel \
-      --config-block "./components/orderer0/genesis.block" \
+      --config-block /opt/gopath/src/github.com/hyperledger/fabric/peer/components/orderer0/genesis.block \
       -o orderer0.glyphchain.local:9443 \
-      --ca-file "./components/orderer0/msp/cacerts/intermediate-chain.crt" \
-      --client-cert "./components/admin/admin.crt" \
-      --client-key "./components/admin/admin.key.pem"
+      --ca-file /opt/gopath/src/github.com/hyperledger/fabric/peer/components/orderer0/msp/cacerts/intermediate-chain.crt \
+      --client-cert /opt/gopath/src/github.com/hyperledger/fabric/peer/components/admin/admin.crt \
+      --client-key /opt/gopath/src/github.com/hyperledger/fabric/peer/components/admin/admin.key.pem
 
     # Join Peer0 to the channel using the container's standard configuration path
     docker exec cli peer channel join \
